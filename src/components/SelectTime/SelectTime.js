6c918bf91c271a6aedf1cf76be5a113a13f3f5dd
NorahJC <norahjeanc@gmail.com>
2019-09-15 17:39:54 -0400

    component that shows basics of screen

---
+++ src/components/SelectTime/SelectTime.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './SelectTime.css';
+
+const SelectTime = () => {
+    return(
+        <div>
+            <div className="bg">
+                <h2>Friday, August 13, 2019</h2>
+            </div>
+            <hr/>
+            {/* <div className="btn-design">
+                <button className="btn-slot"> {times} </button>
+            </div> */}
+        </div>
+
+    );
+}
+
+export default SelectTime;
